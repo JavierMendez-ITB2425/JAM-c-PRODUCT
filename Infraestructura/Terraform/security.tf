74421723626ba379ad9c23c00cf023ff21b4d044
AritzOrtega-ITB2425 <aritz.ortega.7e6@itb.cat>
2026-04-14 17:29:56 +0200

    Create security.tf

---
+++ Infraestructura/Terraform/security.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "sg_firewall" {
+  name        = "sg_firewall"
+  description = "Grupo para el puerto WAN del Firewall"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_servicios" {
+  name        = "sg_servicios"
+  description = "Grupo para redes internas"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
