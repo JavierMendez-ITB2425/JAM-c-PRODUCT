f382037ebdc8a30f2399f5e18da022bcf677dea8
ManuelReyes-ITB2425 <manuel.reyes.7e8@itb.cat>
2026-05-11 00:07:38 +0200

    Update network.tf

--- Infraestructura/Terraform/network.tf
+++ Infraestructura/Terraform/network.tf
@@ -42,3 +42,93 @@ resource "aws_route_table" "rt_public" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
+estadio $ cat network.tf 
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = { Name = "VPC-Estadio" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "IGW-Estadio" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  tags              = { Name = "Subnet-Firewall-WAN" }
+}
+
+resource "aws_subnet" "visitantes" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  tags              = { Name = "Subnet-Visitantes" }
+}
+
+resource "aws_subnet" "gestion" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1a"
+  tags              = { Name = "Subnet-Gestion" }
+}
+
+resource "aws_subnet" "soc" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "us-east-1a"
+  tags              = { Name = "Subnet-SOC" }
+}
+
+# Subnet pública del firewall sale por IGW
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+estadio $ cat network.tf 
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = { Name = "VPC-Estadio" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "IGW-Estadio" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  tags              = { Name = "Subnet-Firewall-WAN" }
+}
+
+resource "aws_subnet" "visitantes" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  tags              = { Name = "Subnet-Visitantes" }
+}
+
+resource "aws_subnet" "gestion" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1a"
+  tags              = { Name = "Subnet-Gestion" }
+}
+
+resource "aws_subnet" "soc" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "us-east-1a"
+  tags              = { Name = "Subnet-SOC" }
+}
+
+# Subnet pública del firewall sale por IGW
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
